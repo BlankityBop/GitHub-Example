e4931bbb9164a3665bca6f8d8ad986b1ccfb28d3
BlankityBop <111342406+BlankityBop@users.noreply.github.com>
2023-09-04 11:07:08 +0200

    Henk's Program.cs
    title

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace GitHub_Example
-{
-    public class Program
-    {
-        static void Main()
-        {
-            
+using static System.Console;
+
+public class Program{
+
+    static void Main(){
+        HogwartsStudent[] hogwartsStudents = {
+            //new HogwartsStudentHenk(100, 20, House.Slytherin),
+        }
+
+        foreach(HogwartsStudent s in hogwartsStudents){
+            WriteLine(s);
         }
     }
+
 }
