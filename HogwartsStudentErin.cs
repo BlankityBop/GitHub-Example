e0057b42b3c667271408d03d3b2ecd900bf490bc
Erin-Phillips <ST10258689@vgconnect.edu.za>
2023-09-04 10:59:30 +0200

    changes

---
+++ HogwartsStudentErin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class HogwartsStudentErin : HogwartsStudent
+    {
+        public override string Name { get { return "Erin"; } }
+        protected override string SayCatchPhrase()
+        {
+            return "Thats what she said";
+        }
+        protected override string CastSpell()
+        {
+            return "Fork";
+        }
+
+        public HogwartsStudentErin() : base (10, 50, House.Ravenclaw)
+        {
+           
+
+        }
+
+    }
