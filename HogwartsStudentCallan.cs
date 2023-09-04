aacc21185deb1791466d1fab92794c8a74e2b4a5
cazz <cazzwastaken@gmail.com>
2023-09-04 10:58:03 +0200

    Added HogwartsStudentCallan

---
+++ HogwartsStudentCallan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HogwartsStudentCallan : HogwartsStudent
+{
+    public override string Name { get { return "Callan"; } }
+
+    public HogwartsStudentCallan() : base(100, 10000000, House.Ravenclaw)
+    {
+
+    }
+
+    protected override string SayCatchPhrase()
+    {
+        return "Cowabunga";
+    }
+    protected override string CastSpell()
+    {
+        return "ABRA CA DABRA MAKE YOU A TURTLE";
+    }
+}
