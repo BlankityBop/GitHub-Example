d8b4dfe0e36acabd0ea2e07ebd324633e41c0566
Spiderbyte321 <143994901+Spiderbyte321@users.noreply.github.com>
2023-09-04 10:52:58 +0200

    My Hogwarts Student class

---
+++ HogwartsStudentJacob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    
+    public class HogwartsStudentJacob : HogwartsStudent
+    {
+        string s = "";
+        private string name = "Jacob";
+        public override string Name { get { return name; } }
+
+        public HogwartsStudentJacob() : base(15, 10, House.Ravenclaw)
+        {
+           
+            
+        }
+
+        protected override string SayCatchPhrase()
+        {
+            s += "Bam";
+            return s;
+        }
+        protected override string CastSpell()
+        {
+            s += "None, there is an intercontinental ballistic missle heading to your address in 5 secs ";
+            return s;
+        }
+
+    }
+
